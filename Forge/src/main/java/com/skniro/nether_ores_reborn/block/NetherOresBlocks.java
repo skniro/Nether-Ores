869dab84e07a7c404306f7aaeffc6c5f3692931b
skniro <46392259+skniro@users.noreply.github.com>
2026-09-11 21:27:50 +0800

    Make ores use correct sound.

--- Forge/src/main/java/com/skniro/nether_ores_reborn/block/NetherOresBlocks.java
+++ Forge/src/main/java/com/skniro/nether_ores_reborn/block/NetherOresBlocks.java
@@ -5,11 +5,14 @@ import com.skniro.nether_ores_reborn.item.MapleItems;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.Identifier;
+import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.eventbus.api.bus.BusGroup;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -21,23 +24,23 @@ import java.util.function.Supplier;
 public class NetherOresBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, NetherOres.MOD_ID);
 
-    public static final RegistryObject<Block> Nether_Coal_Ore = registerBlock("coal_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> Nether_Copper_Ore = registerBlock("copper_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> Nether_Diamond_Ore = registerBlock("diamond_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> Nether_Emerald_Ore = registerBlock("emerald_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> Nether_Gold_Ore = registerBlock("gold_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> Nether_Iron_Ore = registerBlock("iron_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> Nether_Lapis_Ore = registerBlock("lapis_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> Nether_Redstone_Ore = registerBlock("redstone_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings), BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
+    public static final RegistryObject<Block> Nether_Coal_Ore = registerBlock("coal_ore", (settings)-> new DropExperienceBlock(UniformInt.of(0, 2), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> Nether_Copper_Ore = registerBlock("copper_ore", (settings)-> new DropExperienceBlock(ConstantInt.of(0), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> Nether_Diamond_Ore = registerBlock("diamond_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> Nether_Emerald_Ore = registerBlock("emerald_ore", (settings)-> new DropExperienceBlock(UniformInt.of(3, 7), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> Nether_Gold_Ore = registerBlock("gold_ore", (settings)-> new DropExperienceBlock(ConstantInt.of(0), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> Nether_Iron_Ore = registerBlock("iron_ore", (settings)-> new DropExperienceBlock(ConstantInt.of(0), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> Nether_Lapis_Ore = registerBlock("lapis_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> Nether_Redstone_Ore = registerBlock("redstone_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings), BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
 
-    public static final RegistryObject<Block> End_Coal_Ore = registerBlock("end_coal_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> End_Copper_Ore = registerBlock("end_copper_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> End_Diamond_Ore = registerBlock("end_diamond_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> End_Emerald_Ore = registerBlock("end_emerald_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> End_Gold_Ore = registerBlock("end_gold_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> End_Iron_Ore = registerBlock("end_iron_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> End_Lapis_Ore = registerBlock("end_lapis_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final RegistryObject<Block> End_Redstone_Ore = registerBlock("end_redstone_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings), BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
+    public static final RegistryObject<Block> End_Coal_Ore = registerBlock("end_coal_ore", (settings)-> new DropExperienceBlock(UniformInt.of(0, 2), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> End_Copper_Ore = registerBlock("end_copper_ore", (settings)-> new DropExperienceBlock(ConstantInt.of(0), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> End_Diamond_Ore = registerBlock("end_diamond_ore", (settings)-> new DropExperienceBlock(UniformInt.of(3, 7), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> End_Emerald_Ore = registerBlock("end_emerald_ore", (settings)-> new DropExperienceBlock(UniformInt.of(3, 7), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> End_Gold_Ore = registerBlock("end_gold_ore", (settings)-> new DropExperienceBlock(ConstantInt.of(0), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> End_Iron_Ore = registerBlock("end_iron_ore", (settings)-> new DropExperienceBlock(ConstantInt.of(0), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> End_Lapis_Ore = registerBlock("end_lapis_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 5), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final RegistryObject<Block> End_Redstone_Ore = registerBlock("end_redstone_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings), BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
 
 
     public static <B extends Block> RegistryObject<Block> register(String name, Function<BlockBehaviour.Properties, ? extends B> func, BlockBehaviour.Properties props) {
--- Neoforge/src/main/java/com/skniro/nether_ores_reborn/block/NetherOresBlocks.java
+++ Neoforge/src/main/java/com/skniro/nether_ores_reborn/block/NetherOresBlocks.java
@@ -6,11 +6,13 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.Identifier;
+import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
@@ -22,23 +24,23 @@ import java.util.function.Supplier;
 public class NetherOresBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(NetherOres.MOD_ID);
 
-    public static final Supplier<Block> Nether_Coal_Ore = registerBlock("coal_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> Nether_Copper_Ore = registerBlock("copper_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> Nether_Diamond_Ore = registerBlock("diamond_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> Nether_Emerald_Ore = registerBlock("emerald_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> Nether_Gold_Ore = registerBlock("gold_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> Nether_Iron_Ore = registerBlock("iron_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> Nether_Lapis_Ore = registerBlock("lapis_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> Nether_Redstone_Ore = registerBlock("redstone_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings), BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
+    public static final Supplier<Block> Nether_Coal_Ore = registerBlock("coal_ore", (settings)-> new DropExperienceBlock(UniformInt.of(0, 2), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> Nether_Copper_Ore = registerBlock("copper_ore", (settings)-> new DropExperienceBlock(ConstantInt.of(0), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> Nether_Diamond_Ore = registerBlock("diamond_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> Nether_Emerald_Ore = registerBlock("emerald_ore", (settings)-> new DropExperienceBlock(UniformInt.of(3, 7), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> Nether_Gold_Ore = registerBlock("gold_ore", (settings)-> new DropExperienceBlock(ConstantInt.of(0), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> Nether_Iron_Ore = registerBlock("iron_ore", (settings)-> new DropExperienceBlock(ConstantInt.of(0), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> Nether_Lapis_Ore = registerBlock("lapis_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> Nether_Redstone_Ore = registerBlock("redstone_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings), BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).sound(SoundType.NETHERRACK).mapColor(MapColor.NETHER).instrument(NoteBlockInstrument.BASEDRUM));
 
-    public static final Supplier<Block> End_Coal_Ore = registerBlock("end_coal_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> End_Copper_Ore = registerBlock("end_copper_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> End_Diamond_Ore = registerBlock("end_diamond_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> End_Emerald_Ore = registerBlock("end_emerald_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> End_Gold_Ore = registerBlock("end_gold_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> End_Iron_Ore = registerBlock("end_iron_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> End_Lapis_Ore = registerBlock("end_lapis_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings),BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
-    public static final Supplier<Block> End_Redstone_Ore = registerBlock("end_redstone_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings), BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE));
+    public static final Supplier<Block> End_Coal_Ore = registerBlock("end_coal_ore", (settings)-> new DropExperienceBlock(UniformInt.of(0, 2), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> End_Copper_Ore = registerBlock("end_copper_ore", (settings)-> new DropExperienceBlock(ConstantInt.of(0), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> End_Diamond_Ore = registerBlock("end_diamond_ore", (settings)-> new DropExperienceBlock(UniformInt.of(3, 7), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> End_Emerald_Ore = registerBlock("end_emerald_ore", (settings)-> new DropExperienceBlock(UniformInt.of(3, 7), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> End_Gold_Ore = registerBlock("end_gold_ore", (settings)-> new DropExperienceBlock(ConstantInt.of(0), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> End_Iron_Ore = registerBlock("end_iron_ore", (settings)-> new DropExperienceBlock(ConstantInt.of(0), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> End_Lapis_Ore = registerBlock("end_lapis_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 5), settings),BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
+    public static final Supplier<Block> End_Redstone_Ore = registerBlock("end_redstone_ore", (settings)-> new DropExperienceBlock(UniformInt.of(2, 4), settings), BlockBehaviour.Properties.of().requiresCorrectToolForDrops().randomTicks().strength(3.0F, 3.0F).mapColor(MapColor.SAND).instrument(NoteBlockInstrument.BASEDRUM));
 
     private static <B extends Block> DeferredBlock<B> registerBlock(String name, Function<BlockBehaviour.Properties, ? extends B> block, BlockBehaviour.Properties properties) {
         DeferredBlock<B> bDeferredBlock = registerBlockWithoutItem(name, block, properties);
